a53dfb88361210eec223200c50981793c9bd4165
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-10 10:15:38 -0400

    added text area html tags to capture user input

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,31 @@
-import {MyEditor} from "./MyEditor";
+import React, {useRef} from "react";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-export const Home = () =>{
-    return <div>
-        <h1>home</h1>
-        <MyEditor/>
-    </div>
+export const Home = () => {
+    const textAreaContent = useRef(null);
+    const textAreaContent2 = useRef(null);
+    const textAreaContent3 = useRef(null);
+
+    const handleClick = event => {
+        // 👇️ access textarea value
+        console.log(textAreaContent.current.value);
+        console.log(textAreaContent2.current.value);
+        console.log(textAreaContent3.current.value);
+
+    };
+
+    return (
+        <div style={{display: 'flex', flexDirection: 'column', width: '40%', justifyContent: 'space-between'}}>
+            <label htmlFor="message">What does your normal day looks like</label>
+            <textarea ref={textAreaContent} id="message" name="message"/>
+            <br/>
+            <label htmlFor="message2">What are your biggest goals right now </label>
+            <textarea ref={textAreaContent2} id="message2" name="message"/>
+            <br/>
+            <label htmlFor="message3">What are you planning today?</label>
+            <textarea ref={textAreaContent3} id="message3" name="message"/>
+
+            <button onClick={handleClick}>Click</button>
+        </div>
+    );
 }
--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -1,28 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { EditorState } from "draft-js";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-
-
-import { convertToRaw } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import draftToMarkdown from 'draftjs-to-markdown';
-export const MyEditor = () => {
-    const [editorState, setEditorState] = useState(() =>
-        EditorState.createEmpty()
-    );
-    const [textAreaContent, setTextAreaContent] = useState(null);
-    useEffect(() => {
-        setTextAreaContent(draftToMarkdown(convertToRaw(editorState.getCurrentContent())))
-        console.log(textAreaContent)
-    }, [editorState, textAreaContent]);
-    return (
-        <div>
-            <div style={{ border: "1px solid black", padding: '2px', minHeight: '15rem', width: '50%' }}>
-                <Editor
-                    editorState={editorState}
-                    onEditorStateChange={setEditorState}
-                />
-            </div>
-        </div>
-    );
-}
+// import React, { useEffect, useState } from "react";
+// import { EditorState } from "draft-js";
+// import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+//
+//
+// import { convertToRaw } from 'draft-js';
+// import { Editor } from 'react-draft-wysiwyg';
+// import draftToMarkdown from 'draftjs-to-markdown';
+//
+// export const MyEditor = () => {
+//     const [editorState, setEditorState] = useState(() =>
+//         EditorState.createEmpty()
+//     );
+//
+//     const [textAreaContent, setTextAreaContent] = useState(null);
+//     const [textAreaContent2, setTextAreaContent2] = useState(null);
+//     const [textAreaContent3, setTextAreaContent3] = useState(null);
+//     useEffect(() => {
+//         if (textAreaContent === null) {
+//             setTextAreaContent(draftToMarkdown(convertToRaw(editorState.getCurrentContent())))
+//         }else if(textAreaContent2 === null) {
+//
+//             setTextAreaContent2(draftToMarkdown(convertToRaw(editorState.getCurrentContent())))
+//         }
+//         else if(textAreaContent3 !== null) {
+//
+//             setTextAreaContent3(draftToMarkdown(convertToRaw(editorState.getCurrentContent())))
+//         }
+//     }, [editorState, textAreaContent]);
+//     return (
+//         <div>
+//             <div style={{ border: "1px solid black", padding: '2px', minHeight: '15rem', width: '50%' }}>
+//                 <Editor
+//                     editorState={editorState}
+//                     onEditorStateChange={setEditorState}
+//                 />
+//             </div>
+//         </div>
+//     );
+// }
