56cfc8d47b66c7f2723fb7dea6ca6388a617c902
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-10 03:33:26 -0400

    created text field where users can use to enter text input... working on getting the user input from the text area to send it in a post request to the backend

--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Editor } from "react-draft-wysiwyg";
 import { EditorState } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+
+import { convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToMarkdown from 'draftjs-to-markdown';
 export const MyEditor = () => {
     const [editorState, setEditorState] = useState(() =>
         EditorState.createEmpty()
     );
+    const [textAreaContent, setTextAreaContent] = useState(null);
     useEffect(() => {
-        console.log(editorState);
+        setTextAreaContent(draftToMarkdown(convertToRaw(editorState.getCurrentContent())))
+        console.log(textAreaContent)
     }, [editorState]);
     return (
         <div>
-            <h1>React Editors</h1>
-            <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
+            <div style={{ border: "1px solid black", padding: '2px', minHeight: '15rem', width: '50%' }}>
                 <Editor
                     editorState={editorState}
                     onEditorStateChange={setEditorState}
