25f889e07a67b3955e306c9f53654a9061201c20
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-14 21:30:07 -0400

    created a store component to keep track of the state on an entry

---
+++ src/globallVariable/store.js
@@ -0,0 +1,29 @@
+import {configureStore} from "@reduxjs/toolkit";
+
+export const store = configureStore({reducer: userReducer})
+
+//A reducer is a function that receives the current state and an action object, decides how to update the state if necessary, and returns the new state
+function userReducer(state = {
+    "id": null,
+    "EntryOne": null,
+    "EntryTwo": null,
+    "EntryThree": null,
+    "CREATED_AT": null
+}, action) {
+    // Check to see if the reducer cares about this action
+    if (action.type === 'editEntry') {
+        // If so, make a copy of `state`
+        return {
+            ...state,
+            // and update the copy with the new value
+            "id": action.id,
+            "EntryOne": action.EntryOne,
+            "EntryTwo": action.EntryTwo,
+            "EntryThree": action.EntryThree,
+            "CREATED_AT": "",
+            "EDITED_AT": action.EDITED_AT
+        }
+    }
+    // otherwise return the existing state unchanged
+    return state
+}
