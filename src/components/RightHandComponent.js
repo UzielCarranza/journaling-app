ad7cb0986f5264180d1083a176a456e6dd203104
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-15 12:51:47 -0400

    chaged component from class component to functional component to have a better use with hooks

--- src/components/RightHandComponent.js
+++ src/components/RightHandComponent.js
@@ -1,74 +1,60 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {MdOutlineAddCircle} from "react-icons/md";
 import {MyEditor} from "./MyEditor";
+import {store} from "../globallVariable/store";
 
-export class RightHandComponent extends React.Component {
-    constructor(props) {
+export const RightHandComponent = () => {
+    const [firstAnswer, setfirstAnswer] = useState(null);
+    const [secondAnswer, setSecondAnswer] = useState(null);
+    const [thirdAnswer, setThirdAnswer] = useState(null);
+    const [editingMode, setEditingMode] = useState(null);
+
+
+
+    const firstEditorUserInput = (data) => {
+        setfirstAnswer(data);
+    }
+    const secondEditorUserInput = (data) => {
+
+        setSecondAnswer(data);
+    }
+    const thirdEditorUserInput = (data) => {
+        setThirdAnswer(data);
+    }
+    const handleSubmit = () => {
         let today = new Date();
         let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
         let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-        super(props);
-        this.state = {
-            firstAnswer: "",
-            secondAnswer: "",
-            thirdAnswer: "",
-            CREATED_AT: time + " " + date,
-            date_created: date,
+        const newEntry =  {
+            firstJournalEntry: firstAnswer,
+            secondJournalEntry: secondAnswer,
+            thirdJournalEntry: thirdAnswer,
+            CREATED_AT: date + " " + time,
         }
-        this.store = {
-            id: "1"
-        }
-    }
-
-    callback = (value) => {
-        this.state.firstAnswer = value;
-    }
-    callback2 = (value) => {
-
-        this.state.secondAnswer = value;
-    }
-    callback3 = (value) => {
-
-        this.state.thirdAnswer = value;
-    }
-    handleSubmit = () => {
-
-        //TODO: NEEDS POST REQUEST
-        console.log(this.state)
-    }
-
-     render() {
-        return this.state.firstAnswer === "" ? (<div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-            justifyContent: 'space-around',
-            height: '100%',
-            marginLeft: '2%'
-        }}>
-            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'end', width: '61%'}}>
-                <MdOutlineAddCircle className="add" style={{color: 'green', fontSize: '30'}}
-                                    onClick={this.handleSubmit}/>
-            </div>
-            <h4>{this.state.date_created}</h4>
-            <p>What kind of day are you having?</p>
-
-            {this.store.id !== undefined ?
-                <MyEditor parentCallBack={this.callback} editingValue={this.store.EntryOne}/> :
-                <MyEditor parentCallBack={this.callback}/>}
-
-
-            <p>What are your biggest goals right now</p>
-
-            {this.store.id !== undefined ?
-                <MyEditor parentCallBack={this.callback2} editingValue={this.store.EntryTwo}/> :
-                <MyEditor parentCallBack={this.callback2}/>}
-
-            <p>What are you worried about?</p>
-            {this.store.id !== undefined ?
-                <MyEditor parentCallBack={this.callback3} editingValue={this.store.EntryThree}/> :
-                <MyEditor parentCallBack={this.callback3}/>}
-        </div>) : ""
+        console.log(newEntry)
 
     }
+
+    return (<div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        justifyContent: 'space-around',
+        height: '100%',
+        marginLeft: '2%'
+    }}>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'end', width: '61%'}}>
+            <MdOutlineAddCircle className="add" style={{color: 'green', fontSize: '30'}} onClick={handleSubmit}/>
+        </div>
+        <p>What kind of day are you having?</p>
+        <MyEditor parentCallBack={firstEditorUserInput}/>
+
+
+        <p>What are your biggest goals right now</p>
+        <MyEditor parentCallBack={secondEditorUserInput}/>
+
+        <p>What are you worried about?</p>
+        <MyEditor parentCallBack={thirdEditorUserInput}/>
+    </div>)
+
 }
