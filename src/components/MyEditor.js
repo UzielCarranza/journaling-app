62f3eb6d2496df34d78fe5ade3b61ea35db8a2d9
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-14 21:29:20 -0400

    making component to render 2 options, editing and creating entries

--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -5,9 +5,9 @@ import {convertToRaw} from 'draft-js';
 import {Editor} from 'react-draft-wysiwyg';
 import draftToMarkdown from 'draftjs-to-markdown';
 import {BsFillTrashFill} from "react-icons/bs";
+import ContentState from "draft-js/lib/ContentState";
 
-export const MyEditor = ({parentCallBack}) => {
-
+export const MyEditor = ({parentCallBack, editingValue}) => {
     const [editorState, setEditorState] = useState(() =>
         EditorState.createEmpty()
     );
@@ -19,14 +19,34 @@ export const MyEditor = ({parentCallBack}) => {
         parentCallBack(text);
 
     }, [editorState]);
+
+
+    const [contentState, setContentState] = useState(null) // ContentState JSON
+    useEffect(() => {
+        if (editingValue !== undefined && editingValue !== null) {
+            let _contentState = ContentState.createFromText(editingValue);
+            console.log(_contentState)
+            console.log(editingValue)
+            const raw = convertToRaw(_contentState)
+            setContentState(raw)
+        }
+    }, [editingValue])
+
     return (
         <div>
             <div style={{border: "1px solid black", padding: '2px', height: 'auto', width: '60%'}}>
-                <Editor
-                    editorState={editorState}
-                    onEditorStateChange={setEditorState}
-                />
-                <BsFillTrashFill className="delete" style={{color: 'red', marginLeft: '95%', fontSize: '20', alignSelf: 'end'}}
+                {editingValue !== undefined && editingValue !== null ?
+                    <Editor
+                        defaultContentState={contentState}
+                        onEditorStateChange={setEditorState}
+                    />
+                    : <Editor
+                        editorState={editorState}
+                        onEditorStateChange={setEditorState}
+                    />}
+
+                <BsFillTrashFill className="delete"
+                                 style={{color: 'red', marginLeft: '95%', fontSize: '20', alignSelf: 'end'}}
                                  onClick={() => setEditorState(EditorState.createEmpty())}/>
 
             </div>
