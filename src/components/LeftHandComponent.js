40ca2d2cfd5675eb4a236febcaa45491d5dffe86
UzielCarranza <uzielcarranzac97@gmail.com>
2022-09-16 13:25:24 -0400

    completed modal design that display past entries

--- src/components/LeftHandComponent.js
+++ src/components/LeftHandComponent.js
@@ -20,17 +20,30 @@ export const LeftHandComponent = () => {
 
     const modalContent = (newEntry) => {
         return newEntry ? (
-                <div style={{width: '40%', display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', margin: '0 auto'}}>
-                    <p>{newEntry.CREATED_AT}</p>
+                <div style={{
+                    width: '40%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    margin: '0 auto'
+                }}>
+                    <p>{newEntry.CREATED_AT?.slice(0,10)}</p>
 
-                    <p>What kind of day are you having?</p>
-                    <textarea style={{textAlign: 'center'}} value={newEntry.EntryOne}/>
+                    <div className="past-entries--wrapper">
+                        <h4>1.- What kind of day are you having?</h4>
+                        <p style={{textAlign: 'center'}}>{newEntry.EntryOne}</p>
+                    </div>
 
-                    <p>What are your biggest goals right now</p>
-                    <textarea style={{textAlign: 'center'}} value={newEntry.EntryTwo}/>
+                    <div className="past-entries--wrapper">
+                        <h4>2.- What are your biggest goals right now</h4>
+                        <p style={{textAlign: 'center'}}>{newEntry.EntryTwo}</p>
+                    </div>
 
-                    <p>What are you worried about?</p>
-                    <textarea style={{textAlign: 'center'}} value={newEntry.EntryThree}/>
+                    <div className="past-entries--wrapper">
+                        <h4>3.- What are you worried about?</h4>
+                        <p style={{textAlign: 'center'}}>{newEntry.EntryThree}</p>
+                    </div>
                 </div>
             ) :
             <p>loading</p>
